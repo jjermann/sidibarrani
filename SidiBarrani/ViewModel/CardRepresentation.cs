dcc7a83b26ab07861ef939b1437d897d9b1a6305
Jonas Jermann <jjermann2@gmail.com>
2018-12-28 12:05:53 +0100

    more name fixes, add card back and suit images

--- SidiBarrani/ViewModel/CardRepresentation.cs
+++ SidiBarrani/ViewModel/CardRepresentation.cs
@@ -1,18 +1,51 @@
 using System.IO;
+using System.Reactive.Linq;
+using ReactiveUI;
 using SidiBarrani.Model;
 
 namespace SidiBarrani.ViewModel
 {
-    public class CardRepresentation
+    public class CardRepresentation : ReactiveObject
     {
         public Card Card {get;}
-        public string ImageName {get;}
+        private string CardImageSource {get;}
+        private string BackImageSource {get;}
+
+        private bool _isHighlighted;
+        public bool IsHighlighted
+        {
+            get {return _isHighlighted;}
+            set {this.RaiseAndSetIfChanged(ref _isHighlighted, value);}
+        }
+
+        private bool _isFaceUp;
+        public bool IsFaceUp
+        {
+            get {return _isFaceUp;}
+            set {this.RaiseAndSetIfChanged(ref _isFaceUp, value);}
+        }
+
+        private ObservableAsPropertyHelper<string> _imageSource;
+        public string ImageSource
+        {
+            get {return _imageSource.Value;}
+        }
+
+        private CardRepresentation() {}
         public CardRepresentation(Card card)
         {
             Card = card;
             var suitName = card.CardSuit.ToString().ToLowerInvariant();
             var rankName = card.CardRank.ToString().ToLowerInvariant();
-            ImageName = Path.Combine(@"Images", $"{suitName}_{rankName}.png");
+            CardImageSource = Path.Combine(@"Images", $"{suitName}_{rankName}.png");
+            BackImageSource = Path.Combine(@"Images", "card_back.png");
+            IsFaceUp = true;
+
+            this.WhenAnyValue(x => x.IsFaceUp)
+                .Select(isFaceUp => isFaceUp
+                    ? CardImageSource
+                    : BackImageSource)
+                .ToProperty(this, x => x.ImageSource, out _imageSource, null);
         }
     }
 }
