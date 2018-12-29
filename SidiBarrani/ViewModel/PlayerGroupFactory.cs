ccd359e0ceaf0fa59d26e4b157cba2bb6b42e6a3
Jonas Jermann <jjermann2@gmail.com>
2018-12-29 01:07:21 +0100

    add ui interupts for all players

--- SidiBarrani/ViewModel/PlayerGroupFactory.cs
+++ SidiBarrani/ViewModel/PlayerGroupFactory.cs
@@ -14,10 +14,14 @@ namespace SidiBarrani.Model
             Func<Task> confirmTaskGenerator)
         {
             var player1 = PlayerFactory.CreateHumanPlayer("Human1", betActionTaskGenerator, playActionTaskGenerator, confirmTaskGenerator);
-            var player2 = PlayerFactory.CreateRandomPlayer("Random2");
+            // var player1 = PlayerFactory.CreateRandomPlayer("Computer1");
+            var player2 = PlayerFactory.CreateHumanPlayer("Human2", betActionTaskGenerator, playActionTaskGenerator, confirmTaskGenerator);
+            // var player2 = PlayerFactory.CreateRandomPlayer("Computer2");
             var team1 = Team.CreateTeam(player1, player2, "Team1");
-            var player3 = PlayerFactory.CreateRandomPlayer("Random3");
-            var player4 = PlayerFactory.CreateRandomPlayer("Random4");
+            var player3 = PlayerFactory.CreateHumanPlayer("Human3", betActionTaskGenerator, playActionTaskGenerator, confirmTaskGenerator);
+            // var player3 = PlayerFactory.CreateRandomPlayer("Computer3");
+            var player4 = PlayerFactory.CreateHumanPlayer("Human4", betActionTaskGenerator, playActionTaskGenerator, confirmTaskGenerator);
+            // var player4 = PlayerFactory.CreateRandomPlayer("Computer4");
             var team2 = Team.CreateTeam(player3, player4, "Team2");
             var playerGroup = new PlayerGroup(team1, team2);
             return playerGroup;
